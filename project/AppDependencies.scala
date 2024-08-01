6213580e445a6c5847d9feaef255e9dc7f90cb21
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-08-01 10:32:30 +0100

    [ETFE-dependencies] bump bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "8.6.0"
   private val hmrcMongoVersion = "2.2.0"
   private val scalamockVersion  =  "6.0.0"
 
